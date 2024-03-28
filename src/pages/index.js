c04b354333e40a9eb6de913d0e04be5402c1bcda
NEHA <n1ehpatel20@gmail.com>
2024-03-28 12:26:03 +0530

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-      <Head></Head>
-=======
   <h1 className='min-h-100vh'>hi</h1>
->>>>>>> 4b13903b8b6b0851fc3f0154c9c0197e281d3b68
     </>
   )
 }
